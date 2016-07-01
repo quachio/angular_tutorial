e63a0152c4d11df7ee1b5a1b438f4f0636c14146
Hung Quach <quachio@icloud.com>
2016-07-01 15:49:11 -0700

    lesson 11 ng-include completed

---
+++ lesson11-ngInclude/app/app.js
@@ -0,0 +1,37 @@
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+myNinjaApp.config(function () {
+  console.log('config');
+});
+
+myNinjaApp.run(function () {
+  console.log('run');
+});
+
+myNinjaApp.controller('NinjaController', ['$scope', function ($scope) {
+
+    $scope.ninjas = [
+    {
+      name: 'Yoshi',
+      belt: 'green',
+      rate: 50,
+    },
+    {
+      name: 'Cystal',
+      belt: 'Yellow',
+      rate: 30,
+    },
+    {
+      name: 'Shawn',
+      belt: 'black',
+      rate: 10,
+    },
+    {
+      name: 'Ryu',
+      belt: 'gold',
+      rate: 100,
+    },
+  ];
+
+  },
+]);
