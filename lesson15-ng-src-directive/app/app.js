f0c76b8cd74ff388d42c33099fcc092288bcbe93
Hung Quach <quachio@icloud.com>
2016-07-01 21:01:15 -0700

    lesson 15 ng-src completed

---
+++ lesson15-ng-src-directive/app/app.js
@@ -0,0 +1,65 @@
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+myNinjaApp.config(function () {
+  console.log('config');
+});
+
+myNinjaApp.run(function () {
+  console.log('run');
+});
+
+myNinjaApp.controller('NinjaController', ['$scope', function ($scope) {
+    console.log('myNinjaApp.controller');
+    $scope.removeNinja = function (ninja) {
+      var removeNinja = $scope.ninjas.indexOf(ninja);
+      $scope.ninjas.splice(removeNinja, 1); // Start of and remove 1 element
+    };
+
+    $scope.addNinja = function () {
+      $scope.ninjas.push({
+        name: $scope.newninja.name,
+        belt: $scope.newninja.belt,
+        rate: parseInt($scope.newninja.rate),
+        available: true,
+      });
+
+      $scope.newninja.name = '';
+      $scope.newninja.belt = '';
+      $scope.newninja.rate = '';
+    };
+
+    $scope.ninjas = [
+    {
+      name: 'Yoshi',
+      belt: 'green',
+      rate: 50,
+      available: true,
+      thumb: 'content/img/droctocat.png',
+    },
+    {
+      name: 'Cystal',
+      belt: 'Yellow',
+      rate: 30,
+      available: true,
+      thumb: 'content/img/droidtocat.png',
+
+    },
+    {
+      name: 'Shawn',
+      belt: 'black',
+      rate: 10,
+      available: true,
+      thumb: 'content/img/dunetocat.png',
+
+    },
+    {
+      name: 'Ryu',
+      belt: 'gold',
+      rate: 100,
+      available: true,
+      thumb: 'content/img/femalecodertocat.png',
+    },
+  ];
+
+  },
+]);
